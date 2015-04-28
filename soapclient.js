585e47934dc78d41bab4bc596bd39fa6a340e2b8
ThomasKurz <kurzvellberg@gmail.com>
2015-04-28 10:33:26 +0200

    "Result" --> "Response"

--- soapclient.js
+++ soapclient.js
@@ -195,7 +195,7 @@ SOAPClient._sendSoapRequest = function(url, method, parameters, async, callback,
 SOAPClient._onSendSoapRequest = function(method, async, callback, wsdl, req) 
 {
 	var o = null;
-	var nd = SOAPClient._getElementsByTagName(req.responseXML, method + "Result");
+	var nd = SOAPClient._getElementsByTagName(req.responseXML, method + "Response");
 	if(nd.length == 0)
 		nd = SOAPClient._getElementsByTagName(req.responseXML, "return");	// PHP web Service?
 	if(nd.length == 0)
